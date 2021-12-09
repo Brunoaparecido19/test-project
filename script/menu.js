8266811ff8a31a4904c3f2c79bd43abb8b8e36e2
Brunoaparecido19 <ap24201@icloud.com>
2021-12-09 08:10:32 -0300

    adicionado a pagina de atualizar o endereço + input de pesquisa da loja

--- script/menu.js
+++ script/menu.js
@@ -6,3 +6,7 @@ function btnClose(){
    const menu = document.querySelector('.menu');
    menu.style.display = 'none';
 }
+function findItems(){
+   const find = document.querySelector('.find-items');
+   find.style.display = 'block';
+}
