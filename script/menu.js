600c7384a98866f8d2b2cb250b9960e8c3ede1c5
Brunoaparecido19 <ap24201@icloud.com>
2021-12-08 21:21:55 -0300

    frist commit

---
+++ script/menu.js
@@ -0,0 +1,8 @@
+function btnMenu(){
+   const menu = document.querySelector('.menu');
+   menu.style.display = 'block';
+}
+function btnClose(){
+   const menu = document.querySelector('.menu');
+   menu.style.display = 'none';
+}
